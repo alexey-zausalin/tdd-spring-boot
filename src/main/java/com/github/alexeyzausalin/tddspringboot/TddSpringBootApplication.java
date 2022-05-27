8a17a0eddf71f93ae43377c18c5bcb7fe426d1f7
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-27 22:51:05 +0300

    Initial commit

---
+++ src/main/java/com/github/alexeyzausalin/tddspringboot/TddSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.alexeyzausalin.tddspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alexeyzausalin/tddspringboot/TddSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexeyzausalin.tddspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
