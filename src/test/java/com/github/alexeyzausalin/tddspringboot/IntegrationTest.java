5946aa865aa9b681ac258df360134cc2052585e8
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-27 22:54:02 +0300

    Added integration test template

---
+++ src/test/java/com/github/alexeyzausalin/tddspringboot/IntegrationTest.java
@@ -0,0 +1,15 @@
+package com.github.alexeyzausalin.tddspringboot;
+
+import org.junit.jupiter.api.Test;
+
+public class IntegrationTest {
+
+    @Test
+    public void testName() throws Exception {
+        //arrange
+
+        //act
+
+        //assert
+    }
+}
