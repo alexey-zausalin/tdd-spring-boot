c83e48598c9a49a7bd8fa74426b1b6b6bd2ea2c4
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-27 23:22:47 +0300

    Added integration test 'shouldReturnCarDetails' which doesn't pass

--- src/test/java/com/github/alexeyzausalin/tddspringboot/IntegrationTest.java
+++ src/test/java/com/github/alexeyzausalin/tddspringboot/IntegrationTest.java
@@ -1,15 +1,36 @@
 package com.github.alexeyzausalin.tddspringboot;
 
-import org.junit.jupiter.api.Test;
+import com.github.alexeyzausalin.tddspringboot.domain.Car;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IntegrationTest {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    public void testName() throws Exception {
+    public void shouldReturnCarDetails() throws Exception {
         //arrange
 
         //act
+        ResponseEntity<Car> response = restTemplate
+                .getForEntity("/cars/prius", Car.class);
 
         //assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getName()).isEqualTo("prius");
+        assertThat(response.getBody().getType()).isEqualTo("hybrid");
     }
 }
---
+++ src/test/java/com/github/alexeyzausalin/tddspringboot/domain/Car.java
@@ -0,0 +1,15 @@
+package com.github.alexeyzausalin.tddspringboot.domain;
+
+public class Car {
+
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
