ebf88a533cae6d856b4c710cdefcfe69ca210db5
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-27 23:34:24 +0300

    Added test for CarController 'getCar_shouldReturnCar' which doesn't pass

---
+++ src/test/java/com/github/alexeyzausalin/tddspringboot/CarControllerTest.java
@@ -0,0 +1,28 @@
+package com.github.alexeyzausalin.tddspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CarController.class)
+public class CarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getCar_shouldReturnCar() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/car/prius"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("prius"))
+                .andExpect(jsonPath("type").value("hybrid"));
+    }
+}
