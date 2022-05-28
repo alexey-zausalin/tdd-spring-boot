2e3f2835305ba8aae3e0c50bf17bb0b162d22578
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-05-28 23:25:31 +0300

    Added caching with tests

--- src/main/java/com/github/alexeyzausalin/tddspringboot/CarService.java
+++ src/main/java/com/github/alexeyzausalin/tddspringboot/CarService.java
@@ -1,7 +1,10 @@
 package com.github.alexeyzausalin.tddspringboot;
 
 import com.github.alexeyzausalin.tddspringboot.domain.Car;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CarService {
 
     private final CarRepository carRepository;
@@ -10,6 +13,7 @@ public class CarService {
         this.carRepository = carRepository;
     }
 
+    @Cacheable("cars")
     public Car getCarDetails(String name) {
         Car car = carRepository.findByName(name);
         if (car == null) {
--- src/main/java/com/github/alexeyzausalin/tddspringboot/TddSpringBootApplication.java
+++ src/main/java/com/github/alexeyzausalin/tddspringboot/TddSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.github.alexeyzausalin.tddspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TddSpringBootApplication {
 
 	public static void main(String[] args) {
